312bd149e027ecff102c36465a7dbc55160f430c
alumne1daw <alumne1daw@localhost>
2016-03-18 17:07:48 +0100

    Combo box hecho y sql hecho

--- traductorDeColors/src/main/java/U/traductorDeColors/SampleController.java
+++ traductorDeColors/src/main/java/U/traductorDeColors/SampleController.java
@@ -12,11 +12,9 @@ import javafx.event.ActionEvent;
 
 import javafx.scene.control.Label;
 
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 
 public class SampleController {
-	@FXML
-	private ChoiceBox choiceIdioma;
 	@FXML
 	private TextField color;
 	@FXML
@@ -31,19 +29,45 @@ public class SampleController {
 	private TextField icolor2;
 	@FXML
 	private TextField icolor3;
+	@FXML
+	private ComboBox comoIdioma;
 
 	// Event Listener on Button.onAction
 	@FXML
 	public void cercar(ActionEvent event) {
-		Connection conect = null;
+Connection conect = null;
     	
     	try {
 			Class.forName("com.mysql.jdbc.Driver");
 			
 			conect = DriverManager.getConnection("jdbc:mysql://192.168.4.1/traductor", "foot", "ball");
 			
-			String demanat = color.getText(); 
+			comoIdioma.getItems().addAll("Català","Castellà","Francés","Angles");
 			
+			
+			String demanat = color.getText();
+			String idioma = "";
+			
+			switch(comoIdioma.getValue().toString()){
+			case "Català":
+				idioma = "nom";
+				break;
+			case "Castellà":
+				idioma = "castella";
+				break;
+			case "Francés":
+				idioma = "frances";
+				break;
+			case "Angles":
+				idioma = "angles";
+				break;
+			}
+			
+			
+			String consulta = "SELECT angles, castella, nom, frances FROM colors WHERE"+idioma+"="+demanat+";";
+			
+			//Descartar el color del idioma introducido, poner idiomas en los labels y color de ese idioma en
+			//los textFields
 			
 			
 		} catch (ClassNotFoundException e) {
